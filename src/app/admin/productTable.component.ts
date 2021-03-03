e97e4f1fd2501faaae8bf304ace7d69627688471
Pavel Vorobev <p.vorobyev@inbox.ru>
2021-03-03 19:14:55 +0300

    admin basic components

---
+++ src/app/admin/productTable.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  templateUrl: 'productTable.component.html'
+})
+export class ProductTableComponent {
+
+}
--- src/app/model/order.repository.ts
+++ src/app/model/order.repository.ts
@@ -1,16 +1,42 @@
 import {Injectable} from '@angular/core';
 import {Order} from './order.model';
-import {StaticDatasource} from './static.datasource';
 import {Observable} from 'rxjs';
+import {RestDataSource} from './RestDataSource';
 
 @Injectable()
 export class OrderRepository {
   private orders: Order[] = [];
+  private loaded = false;
 
-  constructor(private dataSource: StaticDatasource) {
+  constructor(private dataSource: RestDataSource) {
   }
 
   saveOrder(order: Order): Observable<Order> {
     return this.dataSource.saveOrder(order);
+  }
+
+  loadOrders() {
+    this.loaded = true;
+    return this.dataSource.getOrders()
+      .subscribe(orders => this.orders = orders);
+  }
+
+  getOrders(): Order[] {
+    if (!this.loaded) {
+      this.loadOrders();
+    }
+    return this.orders;
+  }
+
+  updateOrder(order: Order) {
+    this.dataSource.updateOrder(order)
+      .subscribe(ord => this.orders
+        .splice(this.orders.findIndex(o => order.id === o.id), 1, order));
+  }
+
+  deleteOrder(id: number) {
+    this.dataSource.deleteOrder(id)
+      .subscribe(order => this.orders
+        .splice(this.orders.findIndex(o => id === o.id), 1));
   }
 }
